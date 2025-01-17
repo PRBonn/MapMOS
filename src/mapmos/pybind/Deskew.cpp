b14ba897a277132e8ecd696a046d6803bf61f65a
Benedikt Mersch <benedikt.mersch@gmail.com>
2025-01-17 11:06:10 +0100

    Just use the default

--- src/mapmos/pybind/Deskew.cpp
+++ src/mapmos/pybind/Deskew.cpp
@@ -36,9 +36,6 @@ namespace mapmos {
 std::vector<Eigen::Vector3d> Deskew(const std::vector<Eigen::Vector3d> &frame,
                                     const std::vector<double> &timestamps,
                                     const Sophus::SE3d &relative_motion) {
-    static const auto tbb_control_settings = tbb::global_control(
-        tbb::global_control::max_allowed_parallelism, tbb::this_task_arena::max_concurrency());
-
     const std::vector<Eigen::Vector3d> &deskewed_frame = [&]() {
         const auto &omega = relative_motion.log();
         const Sophus::SE3d &inverse_motion = relative_motion.inverse();
